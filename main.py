603058ee4ac8ea8b6e168cbad40abf4e6401c267
Moses Xu <moses@mosesxu.net>
2021-09-18 15:53:36 -0400

    finished move, added some logging stuff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import collections
 
 import util
 
-from typing import dict
+from typing import Dict
 
 DEBUG = True
 LOG_FORMAT = '[%(asctime)s/%(name)s] %(levelname)s %(message)s'
@@ -33,14 +33,19 @@ cons: dict[str, Game] = {}
 game_uid: dict[str, str] = {}
 
 
+def err(sid, msg):
+    sio.emit('error', msg, room=sid)
+    logging.warning(f'Error: User {sid} got error {msg}')
+
+
 @sio.event
 def connect(sid, environ):
-    print('Connection:', sid)
+    logging.info('Connection:', sid)
 
 
 @sio.event
 def disconnect(sid, environ):
-    print('Disconnection:', sid)
+    logging.info('Disconnection:', sid)
 
 
 @sio.event
@@ -50,17 +55,20 @@ def create(sid):
     assert gid not in cons
     cons[gid] = Game([], util.generate_map(GAME_SIZE), False)
 
+    logging.info(f'User {sid} requests new game {uuid}')
+    logging.debug(f'Grid: {cons[gid].grid}')
+
     return gid
 
 
 @sio.event
 def get(sid, uid):
     if uid not in cons:
-        sio.emit('error', f'Game {uid} does not exist', room=sid)
+        err(sid, f'Game {uid} does not exist')
         return
 
     if len(cons[uid].sids) >= 2:
-        sio.emit('error', f'Game {uid} already full', room=sid)
+        err(sid, f'Game {uid} already full')
         return
 
     cons[uid].sids.append(sid)
@@ -69,11 +77,21 @@ def get(sid, uid):
     if len(cons[uid].sids) == 2:
         cons[uid].ready = True
 
+    logging.info(f'User {sid} requested game {uid}, game now has users {cons[uid].sids}, ready state: {cons[uid].ready}')
+
     return cons[uid].grid
 
 
 @sio.event
 def move(sid, x, y):
     if sid not in game_uid:
-        sio.emit('error', f'User {sid} not in game')
+        err(sid, f'User {sid} not in game')
         return
+
+    assert game_uid[sid] in cons
+
+    logging.info(f'User {sid} sent move({x}, {y}) to game {game_uid[sid]}')
+
+    for player_sid in cons[game_uid[sid]].sids:
+        if player_sid != sid:
+            sio.emit('move', {'x': x, 'y': y}, room=player_sid)
