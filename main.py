f05f5bc518c52281bb00556999bf390dbfeb63b2
Moses Xu <moses@mosesxu.net>
2021-09-19 00:17:18 -0400

    delete redundant games, use sanic instead of aiohttp, fix cors issue

--- main.py
+++ main.py
@@ -3,33 +3,41 @@ import colorama
 import coloredlogs
 import socketio
 import uuid
+import threading
+import time
 
 import util
 
 from aiohttp import web
+from datetime import datetime, timedelta
+from sanic import Sanic
 
 DEBUG = True
 LOG_FORMAT = '[%(asctime)s/%(name)s] %(levelname)s %(message)s'
 TIME_FORMAT = '%H:%M:%S'
 
-GAME_SIZE = 8
+GAME_SIZE = 6
+AFK_DURATION = timedelta(minutes=5)
 
 colorama.init()
 coloredlogs.install(fmt=LOG_FORMAT, datefmt=TIME_FORMAT)
 if DEBUG:
     coloredlogs.set_level(logging.DEBUG)
 
-sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins=['*'])
-app = web.Application()
+sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins=[])
+app = Sanic(name='htn21-backend')
+app.config['CORS_AUTOMATIC_OPTIONS'] = True
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 sio.attach(app)
 
 
 # Game globals
 class Game:
     def __init__(self, sids, grid, ready):
-        self.sids = sids
-        self.grid = grid
-        self.ready = ready
+        self.sids: list[str] = sids
+        self.grid: list[list[str]] = grid
+        self.ready: bool = ready
+        self.last_action = datetime.now()
 
 
 cons: dict[str, Game] = {}
@@ -41,6 +49,17 @@ async def err(sid, msg):
     logging.warning(f'Error: User {sid} got error {msg}')
 
 
+def delete_game(uid):
+    logging.info(f'Deleting game {uid}...')
+
+    for player_sid in cons[uid].sids:
+        if player_sid in game_uid:
+            del game_uid[player_sid]
+
+    if uid in cons:
+        del cons[uid]
+
+
 @sio.event
 def connect(sid, environ):
     logging.info(f'Connection: {sid}')
@@ -50,14 +69,18 @@ def connect(sid, environ):
 
 @sio.event
 def disconnect(sid):
+    if sid in game_uid:
+        uid = game_uid[sid]
+        cons[uid].sids.remove(sid)
+        if len(cons[uid].sids) == 0:
+            delete_game(uid)
+
     logging.info(f'Disconnection: {sid}')
 
 
 @sio.event
 def create(sid):
-    gid = str(uuid.uuid4())
-
-    logging.info('eventJSDF OIJSDOFIJSOIFJSIOFJSOFJSDFIODFJ')
+    gid = str(uuid.uuid4())[:8]
 
     assert gid not in cons
     cons[gid] = Game([], util.generate_map(GAME_SIZE), False)
@@ -74,8 +97,8 @@ async def get(sid, uid):
         await err(sid, f'Game {uid} does not exist')
         return
 
-    if len(cons[uid].sids) >= 2:
-        await err(sid, f'Game {uid} already full')
+    if cons[uid].ready:
+        await err(sid, f'Game {uid} already full/started')
         return
 
     cons[uid].sids.append(sid)
@@ -105,10 +128,29 @@ async def move(sid, x, y):
 
     logging.info(f'User {sid} sent move({x}, {y}) to game {game_uid[sid]}')
 
+    cons[game_uid[sid]].last_action = datetime.now()
     for player_sid in cons[game_uid[sid]].sids:
         if player_sid != sid:
             await sio.emit('move', (x, y), to=player_sid)
 
 
+### DISCONNECTING THREAD ###
+def kick_loop():
+    while 1:
+        logging.info('Checking kick loop...')
+        rem_ids = []
+        for k, g in cons.items():
+            if datetime.now() - g.last_action > AFK_DURATION:
+                rem_ids.append(k)
+
+        for rem_id in rem_ids:
+            delete_game(rem_id)
+
+        time.sleep(2 * AFK_DURATION.total_seconds())
+
+
+th = threading.Thread(target=kick_loop)
+th.start()
+
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=8000)
+    app.run('0.0.0.0', 8000)
